711751b501687aa64b691dbd4babed8b96dfc35b
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-07 14:35:10 +0100

    creazione layout di base e commenti js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// calcolare il prezzo del biglietto del treno in base ai kilometri e agli anni
+
+
+// chiedere all'utente gli anni 
+
+// chiedere all'utente il numero dei kilometri che dovrà percorrere
+
+// definire il costo a kilometro
+
+// calcolare il costo totale del biglietto
+
+// se minorenne (sotto i 18 annni) allora applica sconto 20%
+
+// se over 65 applica sconto del 40%
+
+// Comunicare il prezzo totale al cliente 
