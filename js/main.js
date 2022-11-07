f6fa2a3544f81d37133aea926f5a897b71d3c452
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-07 15:16:37 +0100

    Richiesta anni utente

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
 // calcolare il prezzo del biglietto del treno in base ai kilometri e agli anni
 
 
+// variabili
+const costoPrimo = 0.21 
+let anni
+let kilometriDaPercorrere
+let costoTotale
+let scontoMinorenne
+let costoMinorenne
+let scontoOver
+let costoOver
+
 // chiedere all'utente gli anni 
+
+anni = parseInt(prompt("Quanti anni hai?"));
+
+console.log(anni);
 
 // chiedere all'utente il numero dei kilometri che dovrà percorrere
 
-// definire il costo a kilometro
+
 
 // calcolare il costo totale del biglietto
 
+
+
 // se minorenne (sotto i 18 annni) allora applica sconto 20%
 
-// se over 65 applica sconto del 40%
 
 // Comunicare il prezzo totale al cliente 
